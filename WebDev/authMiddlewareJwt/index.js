c0709be2e23e2794aee8283c4e0b6f69d0409301
ishan8727 <ishanch08@gmail.com>
2026-06-18 12:15:40 +0530

    practicing because errors and basics are still weak~

---
+++ WebDev/authMiddlewareJwt/index.js
@@ -0,0 +1,78 @@
+const express = require('express');
+const jwt = require('jsonwebtoken');
+
+const app = express();
+app.use(express.json());
+
+const users = [];
+ JWT_SECRET = 'IamAnoobDev';
+
+app.post('/signup',(req,res)=>{
+    const id = req.body.id;
+    const pass = req.body.pass;
+
+    users.push({
+        id: id,
+        pass: pass
+    })
+
+    res.json({
+        message: "successfully created a user!"
+    })
+})
+
+app.post('/signin', (req,res)=>{
+    const id = req.body.id;
+    const pass = req.body.pass;
+
+    const foundUser = users.find((u)=>{
+        return u.id == id && u.pass == pass
+    })
+    
+    if(foundUser){
+        const token = jwt.sign({
+            id: id
+        }, JWT_SECRET);
+
+        foundUser.token = token;
+
+        res.json({
+            token: token
+        })
+
+    } else{
+        res.json({
+            error:"wrong creds or no user!"
+        })
+    }
+})
+
+
+app.get('/me', (req,res)=>{
+    const token = req.headers.token;
+    const decoded = jwt.verify(token, JWT_SECRET);
+
+    let foundUser;
+    if(decoded.id){
+        for(let i=0 ; i<users.length ; i++){
+            if(decoded.id == users[i].id){
+                foundUser = users[i];
+            }
+        }
+
+        res.json({
+            id: foundUser.id,
+            pass: foundUser.pass
+        })
+    }
+    else{
+        res.json({
+            message:"invalid token passed!"
+        })
+    }
+})
+
+
+app.listen(3000,()=>{
+    console.log('Listening on port 3k');
+})
